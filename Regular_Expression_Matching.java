8e059dad2a3f1c52225c7b236fac0d12d927621a
fateiswar <kokopelli@live.cn>
2013-08-28 14:43:55 +0800

    Update Regular_Expression_Matching.java

--- Regular_Expression_Matching.java
+++ Regular_Expression_Matching.java
@@ -7,14 +7,14 @@ public class Solution {
         }
         if(j == p.length) return false;
         if(j + 1 < p.length && p[j + 1] == '*'){
-            if(isMatch(s, p, i + 1, j + 2)) return true;
+            if(isMatch(s, p, i, j + 2)) return true;
             for(int k = 0; (i + k < s.length) && (s[i + k] == p[j] || p[j] == '.'); k++){
                 if(isMatch(s, p, i + k + 1, j + 2))
                     return true;
             }
             return false;
         }else
-            return (s[i] == p[j] || p[j] == '.') && isMatch(i + 1, j + 1);
+            return (s[i] == p[j] || p[j] == '.') && isMatch(s, p, i + 1, j + 1);
     }
     public boolean isMatch(String s, String p) {
         // Start typing your Java solution below
