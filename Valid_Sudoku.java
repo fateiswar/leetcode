336b373e1190517684a182c8d8e0030577f63f06
fateiswar <kokopelli@live.cn>
2013-09-04 18:44:17 +0800

    Update Valid_Sudoku.java

--- Valid_Sudoku.java
+++ Valid_Sudoku.java
@@ -6,13 +6,13 @@ public class Solution {
         boolean[][] col = new boolean[9][10];
         boolean[][] grid = new boolean[9][10];
         for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++){
-                if(row[i][board[j]) return false;
-                row[i][board[j]] = true;
-                if(col[j][board[j]) return false;
-                col[j][board[j]] = true;
-                if(grid[i / 3 * 3 + j / 3][board[j]]) return false;
-                grid[i / 3 * 3 + j / 3][board[j]] = true;
+            for(int j = 0; j < 9; j++) if(board[i][j] != '.'){
+                if(row[i][board[i][j] - '0']) return false;
+                row[i][board[i][j] - '0'] = true;
+                if(col[j][board[i][j] - '0']) return false;
+                col[j][board[i][j] - '0'] = true;
+                if(grid[i / 3 * 3 + j / 3][board[i][j] - '0']) return false;
+                grid[i / 3 * 3 + j / 3][board[i][j] - '0'] = true;
             }
         return true;
     }
