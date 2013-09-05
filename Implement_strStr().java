dfd3e15a3c57708358f28d524ff0200500d71427
fateiswar <kokopelli@live.cn>
2013-09-05 10:44:15 +0800

    Update Implement_strStr().java

--- Implement_strStr().java
+++ Implement_strStr().java
@@ -5,7 +5,19 @@ public class Solution {
         if(haystack == null) return null;
         if(needle == null) return haystack;
         if(haystack.length() == 0 && needle.length() == 0) return "";
-        for(int i = 0; i < haystack.length(); i++){
+        if(haystack.length() < needle.length()) return null;
+        long mod = (1<<31) - 1, a = 0, b = 0, c = 1;
+        for(int i = 0; i < needle.length(); i++){
+            if(i != 0) c = (c << 8) & mod;
+            a = ((a << 8) + haystack.charAt(i)) & mod;
+            b = ((b << 8) + needle.charAt(i)) & mod;
+        }
+        for(int i = 0; i + needle.length() <= haystack.length(); i++){
+            if(i != 0){
+                a = (a + mod - ((haystack.charAt(i - 1) * c) & mod)) & mod;
+                a = ((a << 8) + haystack.charAt(i + needle.length() - 1)) & mod;
+            }
+            if(a != b) continue;
             boolean ok = true;
             for(int j = 0; j < needle.length(); j++)
                 if(i + j >= haystack.length() || haystack.charAt(i + j) != needle.charAt(j)){
